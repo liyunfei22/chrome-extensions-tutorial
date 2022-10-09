0cb5ff49b06c69ad2cc5de40ea0fe91b9c3bb1e4
李云飞 <liyunfei02@daojia-inc.com>
2022-10-09 17:45:58 +0800

    feat: 消息通信

--- background.js
+++ background.js
@@ -26,3 +26,30 @@ chrome.action.onClicked.addListener((tab) => {
     func: injectedFunction
   });
 });
+// 监听message 并设置徽章
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+  if (request.greeting === "hello") {
+   
+    chrome.storage.local.get(['count'], function (result) {
+      const count = result.count;
+      if (count) {
+        const nextCount = count + 1
+        // text 为 string
+        chrome.action.setBadgeText({ text: String(nextCount) });
+        chrome.action.setBadgeBackgroundColor(
+          {color: [0, 255, 0, 0]},
+        );
+        // 设置storage
+        chrome.storage.local.set({ count: nextCount })
+      } else {
+        chrome.storage.local.set({ count: 1 });
+        chrome.action.setBadgeText({ text: '1' });
+        chrome.action.setBadgeBackgroundColor(
+          {color: [0, 255, 0, 0]},  // Green
+        );
+      }
+    })
+    sendResponse({farewell: "goodbye"});
+  }
+})
--- content/index.js
+++ content/index.js
@@ -1,6 +1,11 @@
+
 var button = document.createElement('button')
-button.innerHTML = 'hi';
-button.addEventListener("click", () =>
-  alert('hello')
-  , false);
+button.innerHTML = 'send message';
+button.addEventListener("click",
+  () => {
+    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+      console.log(response.farewell);
+    });
+  },
+false);
 document.body.appendChild(button);
